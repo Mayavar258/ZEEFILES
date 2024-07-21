5fcf156c4de994098c5eb9eba1bb55df78de7e79
Jigar varma <tushalverma69@gmail.com>
2024-07-21 21:22:30 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -2,12 +2,13 @@
 # (©) Jigarvarma2005
 
 import pymongo
-from config import DB_URI, DB_NAME, FORCE_SUB_CHANNELS
+from config import DB_URI, DB_NAME, FORCE_SUB_CHANNELS, TG_BOT_TOKEN
 
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 
+files_cache = database[TG_BOT_TOKEN.split(":")[0]]
 user_data = database['users']
 fsubs = {x[0]: database[str(x[0])] for x in FORCE_SUB_CHANNELS}
 
@@ -40,3 +41,12 @@ async def full_userbase():
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
+
+
+async def add_cache(file_id: str, caption: str, tg_file_id: str):
+    files_cache.insert_one({'_id': file_id, 'caption': caption, "file_id": tg_file_id})
+    return
+
+async def get_cache(file_id: str):
+    file = files_cache.find_one({'_id': file_id})
+    return file
